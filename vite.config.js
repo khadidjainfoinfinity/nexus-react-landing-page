39771a722076d51a5ea16e13a5fd4f3c309f301b
khadidja <144276956+khadidjainfoinfinity@users.noreply.github.com>
2024-06-15 19:00:30 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-landing-page/', // Update the base path here
+  base: '/nexus-react-landing-page/', 
 })
